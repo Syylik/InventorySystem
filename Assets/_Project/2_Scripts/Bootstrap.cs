66b8ab07a7711eada79c5addf53b0187156a9a98
Syylik <syylik@mail.ru>
2025-10-05 11:14:49 +0300

    TT completed

--- Assets/_Project/2_Scripts/Bootstrap.cs
+++ Assets/_Project/2_Scripts/Bootstrap.cs
@@ -7,15 +7,12 @@ public class Bootstrap : MonoBehaviour
     [SerializeField] Inventory _inventory;
     [SerializeField] InventoryView _inventoryView;
     [SerializeField] InventoryOpener _inventoryOpener;
-    private InventorySave _inventorySave;
 
     [SerializeField] private List<ItemData> _startupItems;
 
     public void Awake()
     {
-        _inventorySave = new InventorySave();
-        if (_inventorySave.HasSave) _inventory = new(_inventorySave.Load());
-        else _inventory = new Inventory();
+        _inventory = new Inventory();
         _inventoryView.Init(_inventory);
 
         Debug.Log("Init Finished. Slots = " + _inventory.Capacity);
@@ -33,6 +30,6 @@ public class Bootstrap : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space)) _inventoryOpener.Toggle();
         if (Input.GetKeyDown(KeyCode.A)) _inventory.TryAddItem(_startupItems[0]);
         if (Input.GetKeyDown(KeyCode.D)) _inventory.TryAddItem(_startupItems[1]);
-        if (Input.GetKeyDown(KeyCode.S)) _inventorySave.Save(_inventory); 
+        if (Input.GetKeyDown(KeyCode.R)) _inventory.RemoveItem(0);
     }
 }
